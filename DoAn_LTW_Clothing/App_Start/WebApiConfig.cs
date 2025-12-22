48cdd991562f60741a6c746194031788e58e83c3
Duyen <duyenduyenmyle@gmail.com>
2025-12-23 00:20:22 +0700

    cai API

---
+++ DoAn_LTW_Clothing/App_Start/WebApiConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace DoAn_LTW_Clothing.App_Start
+{
+    public class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Cấu hình Route cho Web API (thường bắt đầu bằng api/)
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
