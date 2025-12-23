3a3c0c2fdaf00e086ab49499f72127051556f0c7
Nguyễn Huỳnh Thành Danh <danhnht2005@gmail.com>
2025-12-23 16:57:06 +0700

    Create quan ly san pham

---
+++ DoAn_LTW_Clothing/Controllers/Products1Controller.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DoAn_LTW_Clothing.Models;
+
+namespace DoAn_LTW_Clothing.Controllers
+{
+    public class Products1Controller : Controller
+    {
+        private ClothingShopEntities db = new ClothingShopEntities();
+
+        // GET: Products1
+        public ActionResult Index()
+        {
+            var products = db.Products.Include(p => p.Category);
+            return View(products.ToList());
+        }
+
+        // GET: Products1/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // GET: Products1/Create
+        public ActionResult Create()
+        {
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "CatSlug");
+            return View();
+        }
+
+        // POST: Products1/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProductId,CategoryId,ProductName,Slug,Material,MainImage,Summary,IsActive,CreatedAt,UpdatedAt")] Product product, HttpPostedFileBase ImageUpload)
+        {
+            if (ModelState.IsValid)
+            {
+                string path = Server.MapPath("~/assets/images");
+                if (ImageUpload != null)
+                {
+                    product.MainImage = ImageUpload.FileName;
+                    ImageUpload.SaveAs(path + "/" + ImageUpload.FileName);
+                }
+
+                product.CreatedAt = DateTime.Now;
+                product.UpdatedAt = null;
+
+                db.Products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "CatSlug", product.CategoryId);
+            return View(product);
+        }
+
+        // GET: Products1/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "CatSlug", product.CategoryId);
+            return View(product);
+        }
+
+        // POST: Products1/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProductId,CategoryId,ProductName,Slug,Material,MainImage,Summary,IsActive,CreatedAt,UpdatedAt")] Product product, HttpPostedFileBase ImageUpload)
+        {
+            if (ModelState.IsValid)
+            {
+                string path = Server.MapPath("~/assets/images");
+                if (ImageUpload != null)
+                {
+                    product.MainImage = ImageUpload.FileName;
+                    ImageUpload.SaveAs(path + "/" + ImageUpload.FileName);
+                }
+                product.UpdatedAt = DateTime.Now;
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "CatSlug", product.CategoryId);
+            return View(product);
+        }
+
+        // GET: Products1/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Products1/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Product product = db.Products.Find(id);
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
