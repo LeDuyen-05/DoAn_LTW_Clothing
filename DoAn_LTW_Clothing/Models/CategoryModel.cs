f0e0189782809f361cd1dd73143eea8af20e95d9
Nguyễn Huỳnh Thành Danh <danhnht2005@gmail.com>
2025-12-23 17:20:53 +0700

    Create API category

---
+++ DoAn_LTW_Clothing/Models/CategoryModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DoAn_LTW_Clothing.Models
+{
+    public class CategoryModel
+    {
+        public int CategoryId { get; set; }
+        public int GroupId { get; set; }
+        public string CatSlug { get; set; }
+        public string CatName { get; set; }
+        public string Description { get; set; }
+        public int SortOrder { get; set; }
+        public bool IsActive { get; set; }
+        public Nullable<System.DateTime> CreatedAt { get; set; }
+    }
+}
