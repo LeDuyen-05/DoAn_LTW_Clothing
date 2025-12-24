7dab9e5341ffff6f53c18cd1cf31daef7a84b6ea
Nguyễn Huỳnh Thành Danh <danhnht2005@gmail.com>
2025-12-24 19:17:01 +0700

    Update page index admin to Le Duyen

--- DoAn_LTW_Clothing/Controllers/AdminController.cs
+++ DoAn_LTW_Clothing/Controllers/AdminController.cs
@@ -1,17 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using DoAn_LTW_Clothing.Models;
 
 namespace DoAn_LTW_Clothing.Controllers
 {
     public class AdminController : Controller
     {
+        private ClothingShopEntities db = new ClothingShopEntities();
         // GET: Admin
         public ActionResult Index()
         {
-            return View();
+            var data = new HomeAdmin()
+            {
+                totalCategory = db.Categories.Count(),
+                totalOrder = db.Orders.Count(),
+                totalProduct = db.Products.Count(),
+                totalApuser = db.AppUsers.Count(),
+            };
+            return View(data);
         }
 
         public ActionResult GetCategorys()
---
+++ DoAn_LTW_Clothing/Models/HomeAdmin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DoAn_LTW_Clothing.Models
+{
+    public class HomeAdmin
+    {
+        public int totalProduct { get; set; }
+        public int totalCategory { get; set; }
+        public int totalOrder { get; set; }
+        public int totalApuser { get; set; }
+    }
+}
